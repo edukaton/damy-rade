1bbc66a3c1cae7e912f80cc498f4d6c8b79ccf83
Michal Lappo <MTTtiger9@gmail.com>
2018-02-17 14:40:24 +0100

    lalaa

--- src/games/spam/js/Message.js
+++ src/games/spam/js/Message.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 
 class Message extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dropped: false
+        };
+    }
+
+    handleClick() {
+        let dropped = this.state.dropped;
+
+        dropped ? this.setState({dropped: false}) : this.setState({dropped: true});
+    }
+
     render() {
-        let showButtons = !(props.markedAsSpam || props.markedAsGood);
+        let showButtons = !(this.props.markedAsSpam || this.props.markedAsGood);
         let classes = "message";
-        props.markedAsSpam ? classes += " message-spam" : classes += "";
-        props.markedAsGood ? classes += " message-good" : classes += "";
+        this.props.markedAsSpam ? classes += " message-spam" : classes += "";
+        this.props.markedAsGood ? classes += " message-good" : classes += "";
 
         return (
             <div className={classes}>
-                <h2 className="message-title">{props.title}</h2>
-                <span className="message-sender">{props.sender}</span>
-                <span className="message-text">{props.text}</span>
-                <span className="message-date">{props.date}</span>
+                <h2 className="message-title">{this.props.title}</h2>
+                <span className="message-sender">{this.props.sender}</span>
+                <span className="message-text">{this.props.text}</span>
+                <span className="message-date">{this.props.date}</span>
                 <CSSTransitionGroup
                     transitionName="animation"
                     transitionEnterTimeout={500}
                     transitionLeaveTimeout={300}
                 >
-                    {showButtons && <button className="message-button" onClick={() => props.onSpamClick()}>Spam</button>}
-                    {showButtons && <button className="message-button" onClick={() => props.onGoodClick()}>Dobra wiadomość</button>}
+                    {showButtons && <button className="message-button" onClick={() => this.props.onSpamClick()}>Spam</button>}
+                    {showButtons && <button className="message-button" onClick={() => this.props.onGoodClick()}>Dobra wiadomość</button>}
                 </CSSTransitionGroup>
             </div>
         );

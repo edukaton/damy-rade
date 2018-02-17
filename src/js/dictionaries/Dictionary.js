578e2ad820c07f0f14bfcb536be209ba06b6d636
Michal Lappo <MTTtiger9@gmail.com>
2018-02-17 10:46:12 +0100

    yolo

---
+++ src/js/dictionaries/Dictionary.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../css/Dictionary.css';
+
+function Dictionary(props) {
+    return (
+        <div className="dictionary">
+            <h1></h1>
+            <p></p>
+            <h2></h2>
+            <img />
+        </div>
+    );
+}
+
+export default Dictionary;

0947c2cb5e912de4b1545ce7d478e02e2b745050
Daniel Projs <daniel.projs99@gmail.com>
2018-02-18 11:52:36 +0100

    jazdaaaa

--- src/games/arrange/js/examples.js
+++ src/games/arrange/js/examples.js
@@ -90,7 +90,230 @@ const examples = [
                 current: -1
             }
         ]
-    }
+    },
+    {
+        words: [
+            {
+                text: "BEATA MAZUREK",
+                goodOrder: 0,
+            },
+            {
+                goodOrder: 1,
+                text: "PODARŁA"
+            },
+            {
+                goodOrder: 2,
+                text: "OKŁADKĘ"
+            },
+            {
+                goodOrder: 3,
+                text: "TEGO MAGAZYNU"
+            },
+            {
+                goodOrder: -1,
+                text: "ODPOWIADA NA"
+            },
+            {
+                goodOrder: -1,
+                text: "DZIENNIKARKA"
+            },
+            {
+                goodOrder: -1,
+                text: "REAGUJE NA"
+            },
+        ],
+        places: [
+            {
+                text: "",
+                changable: true,
+                order: 0,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 1,
+                current: -1
+            },
+            {
+
+                text: "",
+                changable: true,
+                order: 2,
+                current: -1
+            },
+            {
+
+                text: "",
+                changable: true,
+                order: 3,
+                current: -1 
+            }
+        ]
+    },
+    {
+        words: [
+            {
+                text: "ZWYRODNIALEC",
+                goodOrder: 0,
+            },
+            {
+                goodOrder: 1,
+                text: "TWARZĄ"
+            },
+            {
+                goodOrder: 2,
+                text: "LIDLA"
+            },
+            {
+                goodOrder: 3,
+                text: "SIEĆ"
+            },
+            {
+                goodOrder: 5,
+                text: "POPŁOCHU"
+            },
+            {
+                goodOrder: 6,
+                text: "USUWA"
+            },
+            {
+                goodOrder: 7,
+                text: "REKLAMY"
+            },
+            {
+                goodOrder: -1,
+                text: "PRACODAWCA"
+            },
+            {
+                goodOrder: -1,
+                text: "KTÓRY BYŁ"
+            },
+            {
+                goodOrder: -1,
+                text: "POROZUMENIU"
+            },
+            {
+                goodOrder: -1,
+                text: "ODPIERA"
+            },
+            {
+                goodOrder: -1,
+                text: "ZARZUTY"
+            }
+        ],
+        places: [
+            {
+                text: "",
+                changable: true,
+                order: 0,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 1,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 2,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 3,
+                current: -1
+            },
+            {
+
+                text: "w",
+                changable: false,
+                order: 4,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 5,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 6,
+                current: -1
+            },
+            {
+
+                text: "",
+                changable: true,
+                order: 7,
+                current: -1
+            }
+        ]
+    },
+    {
+        words: [
+            {
+                text: "WYPRZEDZANIE",
+                goodOrder: 0,
+            },
+            {
+                goodOrder: 1,
+                text: "NA PRZEJŚCIU"
+            },
+            {
+                goodOrder: 3,
+                text: "SZYBKA KARMA"
+            },
+            {
+                goodOrder: -1,
+                text: "JAZDA"
+            },
+            {
+                goodOrder: -1,
+                text: "KONIE"
+            },
+            {
+                goodOrder: -1,
+                text: "ZABAWA"
+            },
+            {
+                goodOrder: -1,
+                text: "ODPOCZYWANIE"
+            }
+        ],
+        places: [
+            {
+                text: "",
+                changable: true,
+                order: 0,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 1,
+                current: -1
+            },
+            {
+                text: "i",
+                changable: false,
+                order: 2,
+                current: -1
+            },
+            {
+                text: "",
+                changable: true,
+                order: 3,
+                current: -1
+            },
+        ]
+    },
+    
 ]
 
 export default examples;
